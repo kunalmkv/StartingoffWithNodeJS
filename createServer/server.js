aae3b7503655674db918705738605ddbdb1c8aea
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-01-04 22:31:49 +0530

    parsing request bodies

--- createServer/server.js
+++ createServer/server.js
@@ -1,13 +1,31 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer(function (req, res) {
-
+    const method = req.method;
     const url = req.url;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title><head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit"> send</button></form></body > ');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method == 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+
+        //res.writeHead(302,{});
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     console.log(req.url, req.method, req.headers);
